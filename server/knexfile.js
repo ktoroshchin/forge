1d3632cfab3993cd86d8a211e49cebf17984abd2
Levasseur-Sp <levasseur.sp@gmail.com>
2018-11-13 10:57:17 -0500

    Added a .env file and a knexfile for the configuration of the connection with the psql database

---
+++ server/knexfile.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: process.env.DB_URL,
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DB_URL + '?ssl=true',
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'migrations'
+    }
+  }
+
+};
