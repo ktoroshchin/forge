a18f92b227c25840282b15f31ee2b4ab7c250833
Levasseur-Sp <levasseur.sp@gmail.com>
2018-11-13 10:50:55 -0500

    Made an apollo client that will listen to the server on port 4000
    Changed the App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/'
+})
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <Fragment>
+      Forge Project
+    </Fragment>
+  </ApolloProvider>
+)
 
 export default App;
