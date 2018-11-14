02c2e3f2f9efa6d4beed91584f70017db12c493b
Levasseur-Sp <levasseur.sp@gmail.com>
2018-11-14 09:21:21 -0500

    Made a seed using the packages: uuid and faker

---
+++ server/seeders/20181113233759-users.js
@@ -0,0 +1,19 @@
+'use strict';
+const faker = require('faker');
+const uuid = require('uuid/v4');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('users', [
+      { id: uuid(), first_name: faker.name.firstName(), last_name: faker.name.lastName(), email: faker.internet.email(), password: faker.internet.password() },
+      { id: uuid(), first_name: faker.name.firstName(), last_name: faker.name.lastName(), email: faker.internet.email(), password: faker.internet.password() },
+      { id: uuid(), first_name: faker.name.firstName(), last_name: faker.name.lastName(), email: faker.internet.email(), password: faker.internet.password() },
+      { id: uuid(), first_name: faker.name.firstName(), last_name: faker.name.lastName(), email: faker.internet.email(), password: faker.internet.password() },
+      { id: uuid(), first_name: faker.name.firstName(), last_name: faker.name.lastName(), email: faker.internet.email(), password: faker.internet.password() },
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('users', null, {});
+  }
+};
