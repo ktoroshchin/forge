28a6c1da7c35f2e644b42dcb278fd45268023584
Zachary Lee <zacharylhlee@gmail.com>
2018-11-14 19:30:44 +0000

    Adds input fields for username and password

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -4,6 +4,17 @@ function Login() {
   return (
     <div>
       <h2>Login</h2>
+      <form>
+        <label>
+          Username:
+          <input type="text" name="username" />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
     )
 }
