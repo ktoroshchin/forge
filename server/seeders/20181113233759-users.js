6bff340a5ab5a2487a524fa71b8fbf59b3430673
Levasseur-Sp <levasseur.sp@gmail.com>
2018-11-14 15:20:01 -0500

    Modified the seeds to have one user be the creator of 2 worlds

--- server/seeders/20181113233759-users.js
+++ server/seeders/20181113233759-users.js
@@ -6,7 +6,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('users', null, {}).then(() => {
       queryInterface.bulkInsert('users', [
-        { id: uuid(), username: faker.internet.userName(), first_name: faker.name.firstName(), last_name: faker.name.lastName(), email: faker.internet.email(), password: faker.internet.password() },
+        { id: '7597283c-0a43-4be5-bc73-95280f3c0c5f', username: faker.internet.userName(), first_name: faker.name.firstName(), last_name: faker.name.lastName(), email: faker.internet.email(), password: faker.internet.password() },
         { id: uuid(), username: faker.internet.userName(), first_name: faker.name.firstName(), last_name: faker.name.lastName(), email: faker.internet.email(), password: faker.internet.password() },
         { id: uuid(), username: faker.internet.userName(), first_name: faker.name.firstName(), last_name: faker.name.lastName(), email: faker.internet.email(), password: faker.internet.password() },
         { id: uuid(), username: faker.internet.userName(), first_name: faker.name.firstName(), last_name: faker.name.lastName(), email: faker.internet.email(), password: faker.internet.password() },
