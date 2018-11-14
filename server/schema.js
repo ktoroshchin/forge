5985a766606658a7684cc4d5ca5055d2dbc6d05f
Levasseur-Sp <levasseur.sp@gmail.com>
2018-11-14 15:45:36 -0500

    Added a allWorlds query that returns an array of worlds

--- server/schema.js
+++ server/schema.js
@@ -2,12 +2,19 @@ const typeDefs = `
   type Query {
     allUsers: [User!]!
     findUserById(id: ID!): User!
+    allWorlds: [World!]!
   }
   type User {
     id: ID!
     first_name: String!
     last_name: String!
     email: String!
+  }
+  type World {
+    id: ID!
+    name: String!
+    description: String!
+    user_id: ID!
   }
 `;
 
