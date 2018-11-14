192cdb8b69c37d437d369da9d41aadff38eff5cb
Zachary Lee <zacharylhlee@gmail.com>
2018-11-14 19:53:38 +0000

    Hard-coded worlds to display on homepage

--- client/src/components/homepage.jsx
+++ client/src/components/homepage.jsx
@@ -3,7 +3,19 @@ import React from "react";
 function Homepage() {
   return (
     <div>
-      <h2>Home</h2>
+      <h2>Worlds</h2>
+      <div class="world">
+        <h4>World 1</h4>
+        <p>Description</p>
+      </div>
+      <div class="world">
+        <h4>World 2</h4>
+        <p>Description</p>
+      </div>
+      <div class="world">
+        <h4>World 3</h4>
+        <p>Description</p>
+      </div>
     </div>
   );
 }
