0f31bfee51cf3a4d13baa6bacd68d3f86c8b9a82
Levasseur-Sp <levasseur.sp@gmail.com>
2018-11-14 10:43:40 -0500

    Made the timestamp colums work with different times

--- server/models/user.js
+++ server/models/user.js
@@ -10,8 +10,8 @@ module.exports = (sequelize, DataTypes) => {
     last_name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE,
+    created_at: DataTypes.DATE,
+    updated_at: DataTypes.DATE,
   }, {});
   user.associate = function (models) {
     // associations can be defined here
