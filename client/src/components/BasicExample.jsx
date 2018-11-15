fb94d97b6ab89ea280140d59b82d9e2d9ab0ec3a
Zachary Lee <zacharylhlee@gmail.com>
2018-11-15 16:26:42 +0000

    Rename components with uppercase

--- client/src/components/BasicExample.jsx
+++ client/src/components/BasicExample.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-import SimpleExample from './map-view'
-import Homepage from './homepage'
-import Login from './login'
-import Register from './register'
-
+import BasicMap from './BasicMap'
+import HomePage from './HomePage'
 
 function BasicExample() {
   return (
@@ -20,22 +17,11 @@ function BasicExample() {
           <li>
             <Link to="/map">Map</Link>
           </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
         </ul>
-
         <hr />
-
-        <Route exact path="/" component={Homepage} />
+        <Route exact path="/" component={HomePage} />
         <Route path="/about" component={About} />
         <Route path="/map" component={Map} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-
       </div>
     </BrowserRouter>
   );
@@ -52,7 +38,7 @@ function About() {
 function Map() {
   return (
     <div id="map">
-      <SimpleExample />
+      <BasicMap />
     </div>
   );
 }
