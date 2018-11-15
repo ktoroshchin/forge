6b729fca3bf66936713aea3d4a8c85ace035bbd8
Zachary Lee <zacharylhlee@gmail.com>
2018-11-15 18:38:40 +0000

    Able to take input fields to register new user

--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -1,46 +1,68 @@
-import React from "react";
+import React, {Component} from "react";
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 
-function Register() {
-  const POST_MUTATION = gql`
-    mutation {
-      createNewUser(username: "username", email: "email", password: "password") {
-        id
-        username
-        email
-      }
-    }`
-  return (
-    <div>
-      <h2>Register</h2>
-      <form>
-        <label>
-          First Name:
-          <input type="text" name="first_name" />
-        </label>
-        <label>
-          Last Name:
-          <input type="text" name="last_name" />
-        </label>
-        <label>
-          Email:
-          <input type="text" name="email" />
-        </label>
-        <label>
-          Username:
-          <input type="text" name="username" />
-        </label>
-        <label>
-          Password:
-          <input type="password" name="password" />
-        </label>
-        <Mutation mutation={POST_MUTATION} >
-          {postMutation => <button onClick={postMutation}>Submit</button>}
-        </Mutation>
-      </form>
-    </div>
-  )
+
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: ''
+    }
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+  handleUsernameChange(e) {
+    this.setState({username: e.target.value});
+  }
+  handleEmailChange(e) {
+    this.setState({email: e.target.value});
+  }
+  handlePasswordChange(e) {
+    this.setState({password: e.target.value});
+  }
+  render() {
+    const { username, email, password } = this.state
+    const POST_MUTATION = gql`
+      mutation newMutation($username: String!, $email: String!, $password: String!){
+        createNewUser(username: $username, email: $email, password: $password) {
+          id
+        }
+      }`
+    return (
+      <div>
+        <h2>Register</h2>
+        <form>
+          <label>
+            First Name:
+            <input type="text" name="first_name" />
+          </label>
+          <label>
+            Last Name:
+            <input type="text" name="last_name" />
+          </label>
+          <label>
+            Username:
+            <input onChange={this.handleUsernameChange} type="text" name="username" />
+          </label>
+          <label>
+            Email:
+            <input onChange={this.handleEmailChange} type="text" name="email" />
+          </label>
+          <label>
+            Password:
+            <input onChange={this.handlePasswordChange} type="password" name="password" />
+          </label>
+          <Mutation mutation={POST_MUTATION} variables={{ username, email, password }}>
+            {postMutation => <button onClick={postMutation}>Submit</button>}
+          </Mutation>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default Register;
