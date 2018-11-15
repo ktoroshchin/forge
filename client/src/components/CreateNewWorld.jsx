e196633f161854194ff468a5555402a3d56274bd
Zachary Lee <zacharylhlee@gmail.com>
2018-11-15 20:19:35 +0000

    Adds name and description field to CreateNewWorld

--- client/src/components/CreateNewWorld.jsx
+++ client/src/components/CreateNewWorld.jsx
@@ -5,11 +5,15 @@ import gql from 'graphql-tag';
 function CreateNewWorld() {
   return (
     <div>
-      <h2>CreateNewWorld</h2>
+      <h2>Create A New World</h2>
       <form>
         <label>
           Name:
           <input type="text" name="name" />
+        </label>
+        <label>
+          Description:
+          <input type="text" name="description" />
         </label>
         <input type="submit" value="Submit" />
       </form>
