a6960ae476c83a95f855d149b963ca9198e34e1f
Levasseur-Sp <levasseur.sp@gmail.com>
2018-11-15 10:01:16 -0500

    ixed a bug where the key wouldn't get renamed because of foreign keys constraints

--- server/migrations/20181115142008-change-user_id-to-creator.js
+++ server/migrations/20181115142008-change-user_id-to-creator.js
@@ -2,9 +2,35 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.renameColumn('worlds', 'user_id', 'creator')
+    return queryInterface.removeConstraint('worlds', 'worlds_user_id_fkey').then(() => {
+      return queryInterface.renameColumn('worlds', 'user_id', 'creator').then(() => {
+        return queryInterface.addConstraint('worlds', ['creator'], {
+          type: 'foreign key',
+          name: 'worlds_creator_fkey',
+          references: {
+            table: 'users',
+            field: 'id'
+          },
+          onDelete: 'cascade',
+        });
+      })
+    })
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.renameColumn('worlds', 'creator', 'user_id')
+    return queryInterface.removeConstraint('worlds', 'worlds_creator_fkey').then(() => {
+      return queryInterface.renameColumn('worlds', 'creator', 'user_id').then(() => {
+        return queryInterface.addConstraint('worlds', ['user_id'], {
+          type: 'foreign key',
+          name: 'worlds_user_id_fkey',
+          references: {
+            table: 'users',
+            field: 'id'
+          },
+          onDelete: 'cascade',
+        });
+      })
+    })
   }
 };
+
+// worlds_user_id_fkey on table worlds
