1a2f1cbead1d2c9ed4eba1bd1ea78e3170146e33
Levasseur-Sp <levasseur.sp@gmail.com>
2018-11-15 15:30:33 -0500

    Fixed references problems in the migrations

--- server/migrations/20181115193522-create-maps.js
+++ server/migrations/20181115193522-create-maps.js
@@ -11,7 +11,7 @@ module.exports = {
         type: Sequelize.UUID,
         allowNull: false,
         references: {
-          model: 'users', key: 'id'
+          model: 'worlds', key: 'id'
         }
       },
       url: {
