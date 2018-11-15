22db631fea3d459d3ff27090520f23edeb59e30e
Zachary Lee <zacharylhlee@gmail.com>
2018-11-15 17:20:45 +0000

    Adds mutation to register new user

--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 
 function Register() {
+  const POST_MUTATION = gql`
+    mutation {
+      createNewUser(username: "username", email: "email", password: "password") {
+        id
+        username
+        email
+      }
+    }`
   return (
     <div>
       <h2>Register</h2>
@@ -25,10 +35,12 @@ function Register() {
           Password:
           <input type="password" name="password" />
         </label>
-        <input type="submit" value="Submit" />
+        <Mutation mutation={POST_MUTATION} >
+          {postMutation => <button onClick={postMutation}>Submit</button>}
+        </Mutation>
       </form>
     </div>
-    )
+  )
 }
 
 export default Register;
