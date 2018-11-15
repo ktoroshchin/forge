ceb6d75cf093ae8553811931158d2936ebbb3bcd
Zachary Lee <zacharylhlee@gmail.com>
2018-11-15 21:45:46 +0000

    Adds CreateNewLocation component

---
+++ client/src/components/CreateNewLocation.jsx
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+
+function CreateNewLocation() {
+  return (
+    <div>
+      <h2>Create A New Location</h2>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Population:
+          <input type="text" name="population" />
+        </label>
+        <label>
+          Government:
+          <input type="text" name="government" />
+        </label>
+        <label>
+          Description:
+          <input type="text" name="description" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+    )
+}
+
+export default CreateNewLocation;
