f54fa563df5fd1cdac86df0d4b35f80e29c51a01
Zachary Lee <zacharylhlee@gmail.com>
2018-11-16 19:14:44 +0000

    Adds NavBarDropDown component

---
+++ client/src/components/NavBarDropDown.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Collapse,Navbar,NavbarToggler,NavbarBrand,Nav,UncontrolledDropdown,DropdownToggle,DropdownMenu,DropdownItem } from 'reactstrap';
+
+function GuestNavBar({cookies}) {
+  const userID = cookies.get('userID')
+  if (userID) {
+    return (
+      <Nav className="ml-auto" navbar>
+        <UncontrolledDropdown nav inNavbar>
+          <DropdownToggle nav caret>
+            Hello, {userID}!
+          </DropdownToggle>
+          <DropdownMenu right>
+            <DropdownItem tag={Link} to="/login">
+              Logout
+            </DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </Nav>
+    )
+  } else {
+    return (
+      <Nav className="ml-auto" navbar>
+        <UncontrolledDropdown nav inNavbar>
+          <DropdownToggle nav caret>
+            Hello, Guest!
+          </DropdownToggle>
+          <DropdownMenu right>
+            <DropdownItem tag={Link} to="/login">
+              Login
+            </DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem tag={Link} to="/register">
+              Register
+            </DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </Nav>
+    )
+  }
+}
+
+export default GuestNavBar;
