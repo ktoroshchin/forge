b41a3a0005629f5fb469a9debddbd34ea885b8f5
Macbook <macbook@Macbooks-MacBook-Pro-2.local>
2018-11-16 17:16:15 -0500

    Before a git pull from  origin dev

--- client/src/components/CreateNewLocation.jsx
+++ client/src/components/CreateNewLocation.jsx
@@ -3,23 +3,54 @@ import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-function CreateNewLocation() {
+class CreateNewLocation extends Component {
+  state = {
+    name: null,
+    description: null,
+    marker_id: null,
+    world_id: "3d4f3bc5-a4c9-46a2-a16b-20e039730842",
+    id: "1"
+  }
+
+handleLocation = this.handleLocation.bind(this);
+handleDescription = this.handleDescription.bind(this);
+
+
+handleLocation(e) {
+  this.setState({population: Number(e.target.value)});
+}
+handleDescription(e) {
+  this.setState({description: e.target.value});
+}
+
+render(){
+  const { name, description, marker_id, world_id, id } = this.state
+  const POST_MUTATION = gql`
+    mutation ($name: String!, $description: String, $world_id: ID!, $marker_id: ID, $id: ID!){
+      createNewLocation(name: $name, description: $description, world_id: $world_id, marker_id: $marker_id, id: $id) {
+        id
+      }
+    }`
+
   return (
     <div>
       <Form>
-        <Label for="form">Create a new Town</Label>
+        <Label for="form">Create a new Location</Label>
         <FormGroup>
-          <Label for="town">Name</Label>
-          <Input type="text" name="name" placeholder="location"/>
+          <Label for="Name">Name</Label>
+          <Input onChange={this.handleLocation} type="text" name="name" placeholder="location name"/>
         </FormGroup>
         <FormGroup>
           <Label for="description">Description</Label>
-          <Input type="text" name="description" placeholder="description" />
+          <Input onChange={this.handleDescription} type="text" name="description" placeholder="description" />
         </FormGroup>
-        <Button>Submit</Button>
+        <Mutation mutation={POST_MUTATION} variables={{ name, description, marker_id, world_id, id }}>
+          {postMutation => <Button color="success" onClick={postMutation}>Submit</Button>}
+        </Mutation>
       </Form>
     </div>
     )
+  }
 }
 
 export default CreateNewLocation;
