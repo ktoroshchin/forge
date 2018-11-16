bf196cd10095e9dd125e09d12d663b5a38cfe082
Zachary Lee <zacharylhlee@gmail.com>
2018-11-16 21:24:23 +0000

    Adds onChange event handlers to Login component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, {Component} from "react";
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-function Login({addUserID}) {
-  return (
-    <div>
-      <h2>Login</h2>
-      <Form>
-        <FormGroup>
-          <Label>Username</Label>
-          <Input type="text" name="username" />
-          <Label>Password</Label>
-          <Input type="password" name="password" />
-          <br />
-          <Button color="success" onClick={addUserID("PacMan")}>Submit</Button>
-        </FormGroup>
-      </Form>
-    </div>
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: null,
+      password: null,
+    }
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+  handleUsernameChange(e) {
+    this.setState({username: e.target.value});
+  }
+  handlePasswordChange(e) {
+    this.setState({password: e.target.value});
+  }
+   render() {
+    return (
+      <div>
+        <h2>Login</h2>
+        <Form>
+          <FormGroup>
+            <Label>Username</Label>
+            <Input onChange={this.handleUsernameChange} type="text" name="username" />
+            <Label>Password</Label>
+            <Input onChange={this.handlePasswordChange} type="password" name="password" />
+            <br />
+            <Button color="success">Submit</Button>
+          </FormGroup>
+        </Form>
+      </div>
     )
+  }
 }
 
 export default Login;
