98c4fd4a367579f653f7a3a174392499d566185c
Zachary Lee <zacharylhlee@gmail.com>
2018-11-16 16:01:06 +0000

    Adds reactstrap to homepage

--- client/src/components/World.jsx
+++ client/src/components/World.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Jumbotron } from 'reactstrap';
 
 function World({name, description}) {
   return (
     <div>
+    <Jumbotron>
       <p>World Name: {name}</p>
       <p>Description: {description} </p>
+      </Jumbotron>
     </div>
   );
 }
