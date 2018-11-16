b8fc5cd9ac8e931921da72935c5810ba834be077
Zachary Lee <zacharylhlee@gmail.com>
2018-11-16 17:32:57 +0000

    Adds onChange event listeners for name and description fields

--- client/src/components/CreateNewWorld.jsx
+++ client/src/components/CreateNewWorld.jsx
@@ -5,26 +5,44 @@ import CreateNewCity from './CreateNewCity'
 import CreateNewTown from './CreateNewTown'
 import CreateNewLocation from './CreateNewLocation'
 
-function CreateNewWorld() {
-  return (
-    <div>
-      <h2>Create A New World</h2>
-      <form>
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Description:
-          <input type="text" name="description" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-      <CreateNewCity />
-      <CreateNewTown/>
-      <CreateNewLocation/>
-    </div>
-    )
+class CreateNewWorld extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: null,
+      description: null,
+    }
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+  }
+  handleNameChange(e) {
+    this.setState({name: e.target.value});
+  }
+  handleDescriptionChange(e) {
+    this.setState({description: e.target.value});
+  }
+  render() {
+    const { name, description } = this.state;
+    return (
+      <div>
+        <h2>Create A New World</h2>
+        <form>
+          <label>
+            Name:
+            <input onChange={this.handleNameChange} type="text" name="name" />
+          </label>
+          <label>
+            Description:
+            <input onChange={this.handleDescriptionChange} type="text" name="description" />
+          </label>
+          <br />
+        </form>
+        <CreateNewCity />
+        <CreateNewTown/>
+        <CreateNewLocation/>
+      </div>
+      )
+  }
 }
 
 export default CreateNewWorld;
