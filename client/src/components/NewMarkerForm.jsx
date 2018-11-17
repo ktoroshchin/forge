ddf2ac5d219688ae1beaf4ca5161ffc3c252c2f9
Sonchucks <son.hyun.uk@hotmail.com>
2018-11-17 19:25:44 +0000

    New marker form base set up, just need to implement queries for selection display

--- client/src/components/NewMarkerForm.jsx
+++ client/src/components/NewMarkerForm.jsx
@@ -1,71 +1,96 @@
 import React, { Component } from 'react';
-import { Form, FormGroup, Label, Col, Input, Container, ButtonDropdown, DropdownMenu, DropdownItem, DropdownToggle } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 export default class NewMarkerForm extends Component {
-  constructor(props) {
+constructor(props) {
     super(props);
-    this.toggle = this.toggle.bind(this);
-    this.select = this.select.bind(this);
     this.state = {
+      modal: false,
       dropdownOpen: false,
-      category: "Select Category"
+      value: "",
     };
+
+    this.toggleModal = this.toggleModal.bind(this);
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+    this.select = this.select.bind(this);
   }
 
-  toggle() {
+  toggleDropdown() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  }
+
+  toggleModal() {
     this.setState({
-      dropdownOpen: !this.state.dropdownOpen
+      modal: !this.state.modal
     });
   }
 
   select(event) {
-    console.log(event.target.innerText)
     this.setState({
       dropdownOpen: !this.state.dropdownOpen,
-      category: event.target.innerText
-    });
+      value: event.target.innerText,
+    },
+    () => this.toggleModal())
   }
 
   render() {
     const {submitMarker} = this.props
-    console.log(this.state)
     return (
       <div>
-        <Container className="py-4">
-          <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-          <DropdownToggle>{this.state.category}</DropdownToggle>
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown}>
+          <DropdownToggle caret>
+            Select Category
+          </DropdownToggle>
           <DropdownMenu>
             <DropdownItem onClick={this.select}>City</DropdownItem>
             <DropdownItem onClick={this.select}>Town</DropdownItem>
             <DropdownItem onClick={this.select}>Location</DropdownItem>
           </DropdownMenu>
-        </ButtonDropdown>
-        </Container>
-        { this.state.category == "City" &&
-                <Form>
-                  <FormGroup row>
-                    <Label for="exampleEmail" sm={2} size="lg">Email</Label>
-                    <Col sm={10}>
-                      <Input type="email" name="email" id="exampleEmail" placeholder="lg" bsSize="lg" />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Label for="exampleEmail2" sm={2}>Email</Label>
-                    <Col sm={10}>
-                      <Input type="email" name="email" id="exampleEmail2" placeholder="default" />
-                    </Col>
-                  </FormGroup>
-                </Form>
-        }
-        { this.state.category == "Town" &&
-          <span>Town</span>
-        }
-        { this.state.category == "Location" &&
-          <span>Location</span>
-        }
-        <button onClick={() => {submitMarker()}}>
-          Submit
-        </button>
+        </Dropdown>
+          {this.state.value === 'City' &&
+            <Modal isOpen={this.state.modal} toggle={this.toggleModal} className={this.props.className}>
+            <ModalHeader toggle={this.toggleModal}>Choose City</ModalHeader>
+            <ModalBody>
+              List of Cities
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={() => {submitMarker()}}>
+                Submit
+              </Button>
+              <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+            </ModalFooter>
+            </Modal>
+          }
+          {this.state.value === 'Town' &&
+            <Modal isOpen={this.state.modal} toggle={this.toggleModal} className={this.props.className}>
+            <ModalHeader toggle={this.toggleModal}>Choose Town</ModalHeader>
+            <ModalBody>
+              List of Towns
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={() => {submitMarker()}}>
+                Submit
+              </Button>
+              <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+            </ModalFooter>
+            </Modal>
+          }
+          {this.state.value === 'Location' &&
+            <Modal isOpen={this.state.modal} toggle={this.toggleModal} className={this.props.className}>
+            <ModalHeader toggle={this.toggleModal}>Choose Location</ModalHeader>
+            <ModalBody>
+              List of Locations
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={() => {submitMarker()}}>
+                Submit
+              </Button>
+              <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+            </ModalFooter>
+            </Modal>
+          }
       </div>
     )
   }
