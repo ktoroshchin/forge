4ae721970dd883cecc0b4f46f3d794890923dfa9
Zachary Lee <zacharylhlee@gmail.com>
2018-11-17 21:53:00 +0000

    Created new component for TableofContents

--- client/src/components/DisplayWorldDetails.jsx
+++ client/src/components/DisplayWorldDetails.jsx
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
 import { Button,ListGroup, ListGroupItem } from 'reactstrap';
 import ChooseCategoryToUpdate from './ChooseCategoryToUpdate';
-import CityList from './CityList'
-import TownList from './TownList'
-import LocationList from './LocationList'
+import TableofContents from "./TableofContents"
 import City from './City'
 import Town from './Town'
 import Location from './Location'
@@ -39,67 +37,17 @@ setLocationID(id) {
   render() {
     const worldID = this.props.location.state.id;
     return(
-      <div className="AccordionForWorldPage "  >
-        <ListGroupItem className="world-name ">World</ListGroupItem>
-        <div className="row">
-          <div id="accordion" className="page-align col-md-3">
-            <div className="card" className="listGroup">
-              <div className="card-header" id="headingOne">
-                <button className="btn btn-link category"  data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                  Cities
-                </button>
-              </div>
-              <div id="collapseOne" className="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-                <div className="card-body nopadding">
-                  <ListGroup className="listItemContainer">
-                    <CityList worldID={worldID} setValue={this.setValue} setLocationID={this.setLocationID} />
-                  </ListGroup>
-                </div>
-              </div>
-            </div>
-            <div className="card" className="listGroup">
-              <div className="card-header" id="headingTwo">
-                <h5 className="mb-0">
-                  <button className="btn btn-link category collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    Towns
-                  </button>
-                </h5>
-              </div>
-              <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-                <div className="card-body nopadding">
-                  <ListGroup className="listItemContainer">
-                    <TownList worldID={worldID} setValue={this.setValue} setLocationID={this.setLocationID} />
-                  </ListGroup>
-                </div>
-              </div>
-            </div>
-            <div className="card" className="listGroup">
-              <div className="card-header" id="headingThree">
-                <h5 className="mb-0">
-                  <button className="btn btn-link category collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    Locations
-                  </button>
-                </h5>
-              </div>
-              <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-                <div className="card-body nopadding">
-                  <ListGroup className="listItemContainer">
-                    <LocationList worldID={worldID} setValue={this.setValue} setLocationID={this.setLocationID} />
-                  </ListGroup>
-              </div>
-            </div>
-          </div>
-          <Button onClick={this.handleClick} className="btn btn-success add-world col-md-12">Add World</Button>
-          </div>
-          <div className="col-md-8">
-            {this.state.clicked ? <ChooseCategoryToUpdate /> : null}
-          </div>
+        <div>
+        <TableofContents worldID={worldID} setValue={this.setValue} setLocationID={this.setLocationID}/>
+        <Button onClick={this.handleClick} className="btn btn-success add-world col-md-12">Add World</Button>
+        <div className="col-md-8">
+          {this.state.clicked ? <ChooseCategoryToUpdate /> : null}
+        </div>
           {this.state.value === 'City' && <City cityID={this.state.locationID}/>}
           {this.state.value === 'Town' && <Town  townID={this.state.locationID}/>}
           {this.state.value === 'Location' && <Location locationID={this.state.locationID}/>}
-        </div>
         <ShowMap worldID={worldID} />
-      </div>
+        </div>
     )
   }
 }
---
+++ client/src/components/TableofContents.jsx
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import { Button,ListGroup, ListGroupItem } from 'reactstrap';
+import ChooseCategoryToUpdate from './ChooseCategoryToUpdate';
+import CityList from './CityList'
+import TownList from './TownList'
+import LocationList from './LocationList'
+
+function TableofContents({worldID, setValue, setLocationID}) {
+  return (
+    <div className="AccordionForWorldPage">
+      <ListGroupItem className="world-name">World</ListGroupItem>
+      <div className="row">
+        <div id="accordion" className="page-align col-md-3">
+          <div className="card" className="listGroup">
+            <div className="card-header" id="headingOne">
+              <button className="btn btn-link category" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Cities
+              </button>
+            </div>
+            <div id="collapseOne" className="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+              <div className="card-body nopadding">
+                <ListGroup className="listItemContainer">
+                  <CityList worldID={worldID} setValue={setValue} setLocationID={setLocationID} />
+                </ListGroup>
+              </div>
+            </div>
+          </div>
+          <div className="card" className="listGroup">
+            <div className="card-header" id="headingTwo">
+              <h5 className="mb-0">
+                <button className="btn btn-link category collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  Towns
+                </button>
+              </h5>
+            </div>
+            <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+              <div className="card-body nopadding">
+                <ListGroup className="listItemContainer">
+                  <TownList worldID={worldID} setValue={setValue} setLocationID={setLocationID} />
+                </ListGroup>
+              </div>
+            </div>
+          </div>
+          <div className="card" className="listGroup">
+            <div className="card-header" id="headingThree">
+              <h5 className="mb-0">
+                <button className="btn btn-link category collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  Locations
+                </button>
+              </h5>
+            </div>
+            <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+              <div className="card-body nopadding">
+                <ListGroup className="listItemContainer">
+                  <LocationList worldID={worldID} setValue={setValue} setLocationID={setLocationID} />
+                </ListGroup>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default TableofContents;
