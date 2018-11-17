4733fd7ab28bd518e27584882e9eb69ea1b85e7b
Zachary Lee <zacharylhlee@gmail.com>
2018-11-17 22:06:14 +0000

    Changed button to Add New Element

--- client/src/components/DisplayWorldDetails.jsx
+++ client/src/components/DisplayWorldDetails.jsx
@@ -39,7 +39,7 @@ setLocationID(id) {
     return(
         <div>
         <TableofContents worldID={worldID} setValue={this.setValue} setLocationID={this.setLocationID}/>
-        <Button onClick={this.handleClick} className="btn btn-success add-world col-md-12">Add World</Button>
+        <Button onClick={this.handleClick} className="btn btn-success add-world col-md-12">Add New Element</Button>
         <div className="col-md-8">
           {this.state.clicked ? <ChooseCategoryToUpdate /> : null}
         </div>
