8a34a3df7df3996d8a724374f278286816edf701
Zachary Lee <zacharylhlee@gmail.com>
2018-11-17 15:06:05 +0000

    World name links to world-show page

--- client/src/components/World.jsx
+++ client/src/components/World.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Jumbotron } from 'reactstrap';
+import { Link } from "react-router-dom";
 
 function World({name, description}) {
   return (
     <div>
     <Jumbotron>
-      <p>World Name: {name}</p>
+      <p>World Name:
+        <Link to='/world-show'> {name}</Link>
+      </p>
       <p>Description: {description} </p>
       </Jumbotron>
     </div>
