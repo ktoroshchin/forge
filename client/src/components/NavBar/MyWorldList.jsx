e341073f8efc384e83f5237198872c52ccdbcc5f
Zachary Lee <zacharylhlee@gmail.com>
2018-11-19 19:05:38 +0000

    Redirect to login if guest accesses /my-worlds

--- client/src/components/NavBar/MyWorldList.jsx
+++ client/src/components/NavBar/MyWorldList.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import World from "../World"
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
+import {Redirect} from 'react-router'
 
 function MyWorldList({getUserID}) {
   const userID = getUserID();
@@ -14,20 +15,24 @@ function MyWorldList({getUserID}) {
         description
       }
     }`;
-  return (
-    <div>
-      <h2>My Worlds</h2>
-      <Query query={findUserWorlds}>
-        {({ loading, error, data }) => {
-          if (loading) return <div>Fetching</div>
-          if (error) return <div>Error</div>
-          return (data.findWorldsByCreatorId.map(({ id, name, description }) => (
-            <World key={id} id={id} name={name} description={description} />
-          )));
-        }}
-      </Query>
-    </div>
-  );
+  if (userID) {
+    return (
+      <div>
+        <h2>My Worlds</h2>
+        <Query query={findUserWorlds}>
+          {({ loading, error, data }) => {
+            if (loading) return <div>Fetching</div>
+            if (error) return <div>None</div>
+            return (data.findWorldsByCreatorId.map(({ id, name, description }) => (
+              <World key={id} id={id} name={name} description={description} />
+            )));
+          }}
+        </Query>
+      </div>
+    );
+  } else {
+    return <Redirect to='/login' />
+  }
 }
 
 export default MyWorldList;
