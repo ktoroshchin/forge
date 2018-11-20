78900fb06fda7d8828737371d4c539922629faa4
Shawn-Philippe Levasseur <levasseur.sp@gmail.com>
2018-11-20 17:52:03 -0500

    Modified a mutation name to make more sense

--- server/typeDefs/marker.js
+++ server/typeDefs/marker.js
@@ -3,7 +3,7 @@ const queryType = `
 
 const mutationType = `
   createNewMarker(category: String!, world_id: ID!, name: String!, population: Int, government: String, description: String, map_id: ID, latitude: Float, longitude: Float): Marker!
-  bulkEditMarker(id: ID!, name: String, population: Int, government: String, description: String): Marker!
+  editMarkerInfo(id: ID!, name: String, population: Int, government: String, description: String): Marker!
   placeMarkerOnMap(id: ID!, map_id: ID!, latitude: Float!, longitude: Float!): Marker!
   removeMarkerById(id: ID!): Marker
   removeAllMarkersOnMap(map_id: ID!): Map`;
