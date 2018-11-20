f4d9f56657f05490dcd16b4f40b966af8290d223
Zachary Lee <zacharylhlee@gmail.com>
2018-11-20 17:31:03 +0000

    Creates EditWorld and EditWorldForm components

---
+++ client/src/components/WorldShow/EditWorld.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import {Redirect} from 'react-router'
+import EditWorldForm from './EditWorldForm'
+
+function EditWorld() {
+  const findWorld = gql`
+  query {
+    findWorldById(id: "2fd0df5b-5623-497a-bb21-3d5d9144f618"){
+      id
+      name
+      description
+      creator_id
+    }
+  }`
+  return (
+    <Query query={findWorld}>
+    {({ loading, error, data }) => {
+      if (loading) return <div>Fetching</div>
+      if (error) return <div>Error</div>
+      return <EditWorldForm worldID={data.findWorldById.id} name={data.findWorldById.name} description={data.findWorldById.description} creatorID={data.findWorldById.creator_id} />}}
+    </Query>
+  )
+}
+
+export default EditWorld;
---
+++ client/src/components/WorldShow/EditWorldForm.jsx
@@ -0,0 +1,67 @@
+import React, {Component} from "react";
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import {Redirect} from 'react-router'
+
+class EditWorldForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.name,
+      description: this.props.description,
+      redirect: false
+    }
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+    this.setRedirect = this.setRedirect.bind(this);
+    this.renderRedirect = this.renderRedirect.bind(this);
+  }
+  handleNameChange(e) {
+    this.setState({name: e.target.value});
+  }
+  handleDescriptionChange(e) {
+    this.setState({description: e.target.value});
+  }
+  setRedirect() {
+    this.setState({redirect: true});
+  }
+  renderRedirect() {
+    if (this.state.redirect) {
+      return <Redirect to='/' />
+    }
+  }
+  render() {
+    const { name, description } = this.state;
+    const {id, creator_id} = this.props;
+    const POST_MUTATION = gql`
+      mutation ($id: ID!, $name: String!, $description: String, $creator_id: ID!){
+        bulkEditWorld(id: $id, name: $name, description: $description, creator_id: $creator_id) {
+         id
+        }
+      }`
+    return (
+      <div>
+        <h2>Edit World Details</h2>
+        <Form>
+          <FormGroup>
+            <Label> Name (required)</Label>
+            <Input value={this.state.first_name} onChange={this.handleNameChange} type="text" name="name" />
+            <Label>Description (optional)</Label>
+            <Input value={this.state.last_name} onChange={this.handleDescriptionChange} type="text" name="description" />
+            <br />
+            <Mutation mutation={POST_MUTATION} variables={{ id, name, description, creator_id }}>
+              {(postMutation) =>
+                <Button color="success" onClick={(event)=>{postMutation(event)
+                  .then(()=>{this.setRedirect();})
+                  .catch((error) => {alert("Please input required fields")})}}>Submit</Button>}
+            </Mutation>
+            {this.renderRedirect()}
+          </FormGroup>
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default EditWorldForm;
