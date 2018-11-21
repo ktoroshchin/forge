07638b777166b0cacf15248678cf4f9d66b47483
Levasseur-Sp <levasseur.sp@gmail.com>
2018-11-21 15:39:22 -0500

    Made a simple 'real' seeded world

--- server/seeders/20181114173302-worlds.js
+++ server/seeders/20181114173302-worlds.js
@@ -1,15 +1,21 @@
 'use strict';
-const faker = require('faker');
-const uuid = require('uuid/v4');
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('worlds', null, {}).then(() => {
-      queryInterface.bulkInsert('worlds', [
-        { id: '2fd0df5b-5623-497a-bb21-3d5d9144f618', name: faker.lorem.word(), description: faker.lorem.sentences(2), creator_id: '7597283c-0a43-4be5-bc73-95280f3c0c5f' },
-        { id: uuid(), name: faker.lorem.word(), description: faker.lorem.sentences(2), creator_id: '7597283c-0a43-4be5-bc73-95280f3c0c5f' },
-      ], {});
-    })
+    queryInterface.bulkInsert('worlds', [
+      {
+        id: '2fd0df5b-5623-497a-bb21-3d5d9144f618',
+        name: 'Vilmus',
+        description: 'A world full of surprises at every corner',
+        creator_id: '865a8777-59cf-48d3-95d1-f20195912553'
+      },
+      {
+        id: '9a967301-3467-4197-9e5e-99769f4ba13b',
+        name: 'Ymir',
+        description: 'A cold landscape covered in steamy territories. The temperature is around',
+        creator_id: '865a8777-59cf-48d3-95d1-f20195912553'
+      },
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
