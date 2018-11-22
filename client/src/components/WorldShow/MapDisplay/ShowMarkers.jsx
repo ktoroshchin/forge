a31a6d554e157a05b01a3de057c5eb9e04051eb3
Sonchucks <son.hyun.uk@hotmail.com>
2018-11-22 16:16:05 +0000

    Hooked up more details modal

--- client/src/components/WorldShow/MapDisplay/ShowMarkers.jsx
+++ client/src/components/WorldShow/MapDisplay/ShowMarkers.jsx
@@ -2,9 +2,27 @@ import React, { Component } from "react";
 import { Marker, Popup } from 'react-leaflet';
 import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
-import { Button, ListGroup, ListGroupItem } from 'reactstrap';
+import { Button, ListGroup, ListGroupItem, Modal, ModalHeader } from 'reactstrap';
+import City from "../City";
+import Town from "../Town";
+import Location from "../Location";
+
 
 export default class ShowMarkers extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
   deleteMarker() {
     console.log('Deleting Marker!')
   }
@@ -16,6 +34,7 @@ export default class ShowMarkers extends Component {
               id
               longitude
               latitude
+              category
               name
               description
             }
@@ -32,7 +51,7 @@ export default class ShowMarkers extends Component {
         {({ loading, error, data }) => {
           if (loading) return <div>Fetching</div>
           if (error) return <div>Error</div>
-          return (data.findMarkers.map(({ id, latitude, longitude, name, description }) => (
+          return (data.findMarkers.map(({ id, latitude, longitude, category, name, description }) => (
              <Marker
                   key={id}
                   position={[latitude, longitude]}
@@ -42,12 +61,12 @@ export default class ShowMarkers extends Component {
                       <ListGroupItem className="listItem"><strong>Name</strong>: {name}</ListGroupItem>
                       {description !== null &&
                         <ListGroupItem className="listItem">
-                        {description.length <= 100 &&
+                        {description.length <= 250 &&
                         <span>
                           <strong>Description</strong>: {description}
                         </span>
                         }
-                        {description.length >= 250 &&
+                        {description.length > 250 &&
                         <span>
                           <strong>Description</strong>: {description.slice(0, 249)}...
                         </span>
@@ -56,7 +75,19 @@ export default class ShowMarkers extends Component {
                       }
                     </ListGroup>
                     <br/>
-                    <Button className="btn btn-info btn-sm col-4">More Details</Button>
+                    <Button className="btn btn-info btn-sm col-4" onClick={this.toggleModal}>More Details</Button>
+                    <Modal isOpen={this.state.modal} toggle={this.toggleModal} className={this.props.className}>
+                      <ModalHeader toggle={this.toggleModal}>Details of {name}</ModalHeader>
+                      {category === "City" &&
+                        <City cityID={id} />
+                      }
+                      {category === "Town" &&
+                        <Town townID={id} />
+                      }
+                      {category === "Location" &&
+                        <Location locationID={id} />
+                      }
+                    </Modal>
                     {isUser === true &&
                       <Mutation
                         mutation={POST_MUTATION}
