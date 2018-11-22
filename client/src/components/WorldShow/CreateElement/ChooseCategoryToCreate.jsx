7bbbe9da0de7e82492af8bad6cbb47302732fbb9
Zachary Lee <zacharylhlee@gmail.com>
2018-11-22 15:47:17 +0000

    Removes unnecessary ModalBody

--- client/src/components/WorldShow/CreateElement/ChooseCategoryToCreate.jsx
+++ client/src/components/WorldShow/CreateElement/ChooseCategoryToCreate.jsx
@@ -1,5 +1,5 @@
 import React,  { Component } from "react";
-import { ListGroupItem, Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { ListGroupItem, Button, Modal, ModalHeader } from 'reactstrap';
 import CreateNewCity from './CreateNewCity'
 import CreateNewTown from './CreateNewTown'
 import CreateNewLocation from './CreateNewLocation'
@@ -23,25 +23,21 @@ class ChooseCategoryToCreate extends Component {
           </Button>
           <Modal isOpen={this.state.modal === "1"} toggle={this.toggleModal} >
             <ModalHeader toggle={this.toggleModal}>Create a New City</ModalHeader>
-              <CreateNewCity worldID={this.state.worldID} />
+            <CreateNewCity worldID={this.state.worldID} />
           </Modal>
           <Button className="category-button col-12" onClick={this.toggleModal} value={"2"} >
             New Town
           </Button>
           <Modal isOpen={this.state.modal === "2"} toggle={this.toggleModal} >
             <ModalHeader toggle={this.toggleModal}>Create a New Town</ModalHeader>
-            <ModalBody>
-              <CreateNewTown worldID={this.state.worldID} toggleModal={this.toggleModal} />
-            </ModalBody>
+            <CreateNewTown worldID={this.state.worldID} toggleModal={this.toggleModal} />
           </Modal>
           <Button className="category-button col-12" onClick={this.toggleModal} value={"3"} >
             New Location
           </Button>
           <Modal isOpen={this.state.modal === "3"} toggle={this.toggleModal} >
             <ModalHeader toggle={this.toggleModal}>Create a New Location</ModalHeader>
-            <ModalBody>
-              <CreateNewLocation worldID={this.state.worldID} />
-            </ModalBody>
+            <CreateNewLocation worldID={this.state.worldID} />
           </Modal>
         </div>
       </div>
