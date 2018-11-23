3451a20b9082ad0bfd309909ef0fc5fe14362aa6
Levasseur-Sp <levasseur.sp@gmail.com>
2018-11-23 11:12:45 -0500

    Removed the first_name and last_name columns in the users table

--- server/seeders/20181113233759-users.js
+++ server/seeders/20181113233759-users.js
@@ -4,8 +4,8 @@ const bcrypt = require('bcrypt-nodejs');
 module.exports = {
   up: (queryInterface, Sequelize) => {
     queryInterface.bulkInsert('users', [
-      { id: '7597283c-0a43-4be5-bc73-95280f3c0c5f', username: 'ForgeAdmin', first_name: 'Forge', last_name: 'Admin', email: 'forge-admin@forge.com', password: bcrypt.hashSync('admin-password') },
-      { id: '865a8777-59cf-48d3-95d1-f20195912553', username: 'TheCreator', first_name: null, last_name: null, email: 'icreate94@gmail.com', password: bcrypt.hashSync('supersecure') },
+      { id: '7597283c-0a43-4be5-bc73-95280f3c0c5f', username: 'ForgeAdmin', email: 'forge-admin@forge.com', password: bcrypt.hashSync('admin-password') },
+      { id: '865a8777-59cf-48d3-95d1-f20195912553', username: 'TheCreator', email: 'icreate94@gmail.com', password: bcrypt.hashSync('supersecure') },
     ], {});
   },
 
