62bf9aa26b4af6b066ebe42f81de72343b798b32
Zachary Lee <zacharylhlee@gmail.com>
2018-11-23 22:03:45 +0000

    Adds styling to worlds

--- client/src/components/World.jsx
+++ client/src/components/World.jsx
@@ -4,27 +4,60 @@ import { Link } from "react-router-dom";
 
 function World({world_id, name, description, creator_id, maps}) {
   if (maps.length === 0) {
-    return (
-      <Link to={{pathname: "/world-show", state: {worldID: world_id, creatorID: creator_id}}}>
-        <Jumbotron>
-          <p>World Name: {name}</p>
-          {description && <p>Description: {description}</p>}
-        </Jumbotron>
-      </Link>
-    );
-  } else {
-    const backgroundImage = {
-      backgroundImage: `url('${maps[0].url}')`,
-      backgroundRepeat: 'no-repeat',
-      backgroundAttachment: 'relative',
-      backgroundPosition: 'center',
+    const style = {
+      image: {
+        backgroundColor: "grey",
+        height: "200px",
+        borderRadius: "10px",
+        marginBottom: "1em"
+      },
+      content: {
+        height: '100%',
+        width: '100%',
+        color: 'black',
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        padding: '3em',
+        textAlign: "center",
+      }
     }
     return (
       <Link to={{pathname: "/world-show", state: {worldID: world_id, creatorID: creator_id}}}>
-      <Jumbotron style={backgroundImage}>
-        <p>World Name: {name}</p>
-        {description && <p>Description: {description}</p>}
-      </Jumbotron>
+        <div style={style.image}>
+          <div style={style.content}>
+            <h3>{name}</h3>
+            {description && <p>Description: {description}</p>}
+          </div>
+        </div>
+      </Link>
+    );
+  } else {
+    const style = {
+      image: {
+        backgroundImage: `url('${maps[0].url}')`,
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'relative',
+        backgroundPosition: 'center',
+        height: "200px",
+        borderRadius: "10px",
+        marginBottom: "1em"
+      },
+      content: {
+        height: '100%',
+        width: '100%',
+        color: 'black',
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        padding: '3em',
+        textAlign: "center",
+      }
+    }
+    return (
+      <Link to={{pathname: "/world-show", state: {worldID: world_id, creatorID: creator_id}}}>
+      <div style={style.image}>
+        <div style={style.content}>
+          <h3>{name}</h3>
+          {description && <p>Description: {description}</p>}
+        </div>
+      </div>
     </Link>
     )
   }
