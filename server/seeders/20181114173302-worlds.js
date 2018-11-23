46dc1c95f9afd319be9ee0ee2d8ed770bd1d7e24
Levasseur-Sp <levasseur.sp@gmail.com>
2018-11-23 14:21:38 -0500

    Added new seeds for the second world that had nothing associated to it

--- server/seeders/20181114173302-worlds.js
+++ server/seeders/20181114173302-worlds.js
@@ -11,8 +11,8 @@ module.exports = {
       },
       {
         id: '9a967301-3467-4197-9e5e-99769f4ba13b',
-        name: 'Ymir',
-        description: 'A cold landscape covered in steamy territories. The temperature is around',
+        name: 'Ymir, the new Earth',
+        description: 'Planet Earth, after years of getting it\'s ressources extracted, is now a frozen landscape covered in steam. The temperature is around minus 20 degrees celcius during the summer',
         creator_id: '865a8777-59cf-48d3-95d1-f20195912553'
       },
     ], {});
