834cee517ccc8b5fc817a09aa23cc9ed04efe04e
Levasseur-Sp <levasseur.sp@gmail.com>
2018-11-23 14:21:38 -0500

    Added new seeds for the second world that had nothing associated to it

--- server/seeders/20181116213718-maps.js
+++ server/seeders/20181116213718-maps.js
@@ -11,7 +11,14 @@ module.exports = {
           world_map: true,
           height: 1800,
           width: 2700
-        },
+        }, {
+          id: '4b64f613-2cd6-4f4a-8ff5-35a991e73d4f',
+          url: 'https://img00.deviantart.net/c948/i/2016/326/e/5/earth_world_map_blueprint_poster_by_turquoiserabbit-dap8yi8.png',
+          world_id: '9a967301-3467-4197-9e5e-99769f4ba13b',
+          world_map: true,
+          height: 1067,
+          width: 1600
+        }
       ], {});
     })
   },
