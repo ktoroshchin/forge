fe54dcdc3503a18437833db7fd5434a60b0d824d
Macbook <macbook@Macbooks-MacBook-Pro-2.local>
2018-11-24 16:16:05 -0500

    Styling

--- client/src/components/WorldShow/DisplayWorldDetails.jsx
+++ client/src/components/WorldShow/DisplayWorldDetails.jsx
@@ -207,7 +207,7 @@ refreshComponent() {
                   </Modal>
                   </div>
                 }
-                <ShowMap worldID={worldID} isUser={this.state.isUser} />
+              
               </div>
             }
             {(this.state.value !== '' || this.state.clicked) &&
