aa2e534d71c9b1da0d5d6c02802a3d5cc860f7b5
Levasseur-Sp <levasseur.sp@gmail.com>
2018-11-26 15:06:48 -0500

    Added a creator_id field to the searchWorlds

--- server/typeDefs/Query.js
+++ server/typeDefs/Query.js
@@ -2,7 +2,7 @@ module.exports = Query = `type Query {
   findUsers(id: ID, username: String): [User!]
 
   findWorlds(id: ID, creator_id: ID, name: String): [World!]
-  searchWorlds(name: String!): [World!]
+  searchWorlds(creator_id: ID, name: String!): [World!]
 
   findWorldMap(world_id: ID): WorldMap
   findMarkerMap(marker_id: ID): MarkerMap
